7936df0d80c088a703d17d425a76186c2353d3c1
JamesRees <jarees@gmail.com>
2015-10-28 15:08:15 +0000

    fix clockwork implementation

--- config/clock.rb
+++ config/clock.rb
@@ -6,10 +6,11 @@ module Clockwork
 
   Clockwork.manager = DatabaseEvents::Manager.new
 
-  sync_database_events model: ClockworkEvent, every: 1.minute do |job|
+  sync_database_events model: ClockworkEvent, every: 1.minute do |clockwork_event|
 
     # Because job is cached we need to retrieve a fresh copy from DB to see if it is indeed still running
-    if !ClockworkEvent.find(job.id).running
+    job = ClockworkEvent.find(job.id)
+    if !job.running
         if job.queue
             job.delay(:queue => job.queue).perform
         else
