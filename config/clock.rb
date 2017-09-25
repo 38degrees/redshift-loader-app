afc39071e1b923cdd06210087ef6655741fb1911
asibs <andrew.s@38degrees.org.uk>
2017-09-25 09:28:17 +0100

    Remove the Clockwork if? method from ClockworkEvent. The bug this was trying to prevent in Clockwork should have less impact now that we're using Sidekiq with Unique Job

--- config/clock.rb
+++ config/clock.rb
@@ -14,13 +14,13 @@ module Clockwork
   #NOTE: According to Clockwork docs, the 'every' in sync_database_events just
   # reloads the models in case of changes, and doesn't affect the frequency with
   # which jobs are executed - but IN FACT, it does. Whenever it reloads, it also
-  # restarts jobs, so if the frequency of a job is set higher than the sync_freq
+  # restarts jobs, so if a job should run less frequently than the sync_freq
   # here, the job will still run at least every sync_freq seconds.
   # 
   # (eg. if you wanted a job that runs once every 5 mins, BOTH the frequency of
   # the ClockworkEvent in the DB AND the sync_freq here need to be >= 5 mins)
   # 
-  # :'(
+  # This should be less of an issue now that we're using Sidekiq with Unique Jobs
   sync_freq = (ENV['SYNC_DB_EVENTS_FREQUENCY_MINS'] || 1).to_i
   
   sync_database_events model: ClockworkEvent, every: sync_freq.minutes do |clockwork_event|
--- models/clockwork_event.rb
+++ models/clockwork_event.rb
@@ -16,21 +16,6 @@ class ClockworkEvent < ActiveRecord::Base
         running: {:type => :check_box, :edit => false}
       }
     end
-    
-    # Don't run if we last succeeded within frequency seconds
-    #NOTE: This shouldn't be necessary, but is due to Clockwork bug... See long comment in config/clock.rb
-    def if?(time)
-        return true if !last_succeeded_at.present?
-        
-        time_since_last_success = (time - last_succeeded_at.to_datetime)
-        if  time_since_last_success >= frequency.to_i.seconds
-            logger.debug "It's been #{time_since_last_success} since last success of #{self.id} - #{self.name}, frequency is #{frequency} s, triggering job"
-            return true
-        else
-            logger.debug "It's been #{time_since_last_success} since last success of #{self.id} - #{self.name}, frequency is #{frequency} s, NOT triggering job"
-            return false
-        end
-    end
 
     def schedule
       if running && (DateTime.now - last_run_at.to_datetime) * 1.day > 61.seconds
