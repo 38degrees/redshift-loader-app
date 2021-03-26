8c1840ea53c91147ca196c8d068d02a4a03ea83f
James Hulme <james.h@38degrees.org.uk>
2021-03-26 16:05:28 +0000

    Use a guard in airbrake.rb
    Less nesting ftw!

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,24 +1,23 @@
+return if ENV['AIRBRAKE_PROJECT_ID'].nil?
 
-if ENV['AIRBRAKE_PROJECT_ID']
-  # :nocov:
-  Padrino.before_load do
-    require 'airbrake'
-    Airbrake.configure do |config|
-      config.project_id = ENV['AIRBRAKE_PROJECT_ID']
-      config.project_key = ENV['AIRBRAKE_PROJECT_KEY']
+# :nocov:
+Padrino.before_load do
+  require 'airbrake'
+  Airbrake.configure do |config|
+    config.project_id = ENV['AIRBRAKE_PROJECT_ID']
+    config.project_key = ENV['AIRBRAKE_PROJECT_KEY']
 
-      config.host = ENV['AIRBRAKE_HOST'] if ENV['AIRBRAKE_HOST']
+    config.host = ENV['AIRBRAKE_HOST'] if ENV['AIRBRAKE_HOST']
 
-      config.blocklist_keys = ['password']
+    config.blocklist_keys = ['password']
 
-      config.root_directory = Padrino.root
-      config.environment = Padrino.env
-      config.ignore_environments = [:test]
-    end
-
-    Airbrake.add_filter do |notice|
-      notice.ignore! if notice.stash[:exception].is_a?(Sinatra::NotFound)
-    end
+    config.root_directory = Padrino.root
+    config.environment = Padrino.env
+    config.ignore_environments = [:test]
   end
-  # :nocov:
+
+  Airbrake.add_filter do |notice|
+    notice.ignore! if notice.stash[:exception].is_a?(Sinatra::NotFound)
+  end
 end
+# :nocov:
