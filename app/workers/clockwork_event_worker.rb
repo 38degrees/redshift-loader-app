90d1e92bf81e8d066d86cbfd1a1927d0e1c2b1ac
Cindy Xie <cindyxie89@gmail.com>
2025-07-16 12:09:16 +0100

    removing sidekiq deprecated unique_args and unique and re-writing sidekiq workers to be compatible with sidekiq_unique_jobs v8

--- app/workers/clockwork_event_worker.rb
+++ app/workers/clockwork_event_worker.rb
@@ -7,12 +7,12 @@ class ClockworkEventWorker
   #
   # Also, only allow one instance of each ClockworkEvent to be running at a time using sidekiq-unique-jobs
   sidekiq_options retry: 1,
-                  unique: :until_executed,
-                  unique_args: :unique_args,
-                  lock_expiration: (1 * 60 * 60)  # 1 hour
+                  lock: :until_executed,
+                  lock_args_method: :lock_args,
+                  lock_expiration: 1.hour
 
-  def self.unique_args(args)
-    [ args[0] ]
+  def self.lock_args(args)
+    [args[0]]  # args is an array of perform method arguments, so we pick the first one
   end
   
   def perform(clockwork_event_id)
