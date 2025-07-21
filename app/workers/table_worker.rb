bef6685929d1b44d8f3d3cf935b951f3fdf72e2c
Cindy Xie <cindyxie89@gmail.com>
2025-07-21 11:12:12 +0100

    putting back middleware as it was logging jid and putting back a comment

--- app/workers/table_worker.rb
+++ app/workers/table_worker.rb
@@ -20,14 +20,15 @@ sidekiq_options retry: 1,
     @table_id = table_id
     @lock_name = lock_name
   
-    logger.info "[perform] START for table_id=#{@table_id}, lock_name=#{@lock_name}"
+    logger.info "[perform] START for jid=#{jid}, table_id=#{@table_id}, lock_name=#{@lock_name}"
   
     t = Table.find(@table_id)
     rows_copied = t.copy_now
   
     run_again = (rows_copied >= t.import_row_limit)
-    logger.info "[perform] FINISH for table_id=#{@table_id}, lock_name=#{@lock_name}, run_again=#{run_again}"
+    logger.info "[perform] FINISH for jid=#{jid}, table_id=#{@table_id}, lock_name=#{@lock_name}, run_again=#{run_again}"
   
+    # Should schedule again if we hit the row limit, as there are more rows to copy
     if run_again
       logger.info "[perform] run_again=true — re-enqueuing for table_id=#{@table_id}, lock_name=#{@lock_name}"
       TableWorker.perform_async(@table_id, @lock_name)
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,26 @@
 require_relative '../../lib/sidekiq/clear_active_connections'
-require 'sidekiq-unique-jobs'
+require "sidekiq-unique-jobs"
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
 
-  config.logger.level = Logger.const_get((ENV['LOG_LEVEL'] || 'info').upcase)
+  config.logger.level = Logger.const_get( (ENV['LOG_LEVEL'] || 'info').upcase )
 
   config.server_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Server
     chain.add Sidekiq::ClearActiveConnections
   end
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+
+  SidekiqUniqueJobs::Server.configure(config)
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
 end
